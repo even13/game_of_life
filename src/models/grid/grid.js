fafa101deaf60a71d97fdab2413cf32c29392cb0
Raluca Ciucu <raluca.ciucu@gmail.com>
2020-02-04 17:51:08 +0000

    feat: 3x3 spinner cells react to evolution rules

--- src/models/grid/grid.js
+++ src/models/grid/grid.js
@@ -3,9 +3,37 @@ class Grid {
     constructor(initialCells = []) {
         this.initialCells = initialCells
         this.isEvolved = false
-        this.currentGrid = []
+        this.currentGrid = null
     }
     render = () => {
+        // const newGrid = []
+        // let newRow;
+        // for (let y = 0; y < 3; y++) {
+        //     newRow = []
+        //     for (let x = 0; x < 3; x++) {
+        //         if (JSON.stringify(this.initialCells).includes(JSON.stringify([x,y]))) {
+        //             newRow.push('*')
+        //         } else {
+        //             newRow.push('-')
+        //         }
+        //     }
+        //     newGrid.push(newRow)
+        // }
+        // this.currentGrid = newGrid
+        if (this.currentGrid == null) {
+          return [
+            ['-', '-', '-'],
+            ['-', '-', '-'],
+            ['-', '-', '-']
+        ]
+        }
+        return this.currentGrid
+        
+        
+    }
+
+    place_cells = (cellArray) => {
+        this.initialCells = cellArray
         const newGrid = []
         let newRow;
         for (let y = 0; y < 3; y++) {
@@ -20,21 +48,16 @@ class Grid {
             newGrid.push(newRow)
         }
         this.currentGrid = newGrid
-        return newGrid
-    }
-
-    place_cells = (cellArray) => {
-        this.initialCells = cellArray
     }
 
     evolve = () => {
       const newGrid = []
-      this.render().forEach((row, y) => {
+      this.currentGrid.forEach((row, y) => {
         let newRow = []
         row.forEach((elt, x) => {
           let liveCellCount = 0
           for(var i = 0; i < this.neighbours(y, x).length; i++) {
-            if(i === "*") liveCellCount++;
+            if(this.neighbours(y, x)[i] === "*") liveCellCount++;
           }
           let newElt = this.newState(elt, liveCellCount)
           newRow.push(newElt)
@@ -47,10 +70,14 @@ class Grid {
     neighbours = (x, y) => {
       let size = this.currentGrid.length
       return [
-      this.currentGrid[(size - (x - 1)) % size][(size - (y - 1)) % size], this.currentGrid[(size - x) % size][(size - (y - 1)) % size],
-      this.currentGrid[(size - (x + 1)) % size][(size - (y - 1)) % size], this.currentGrid[(size - (x - 1)) % size][(size - y) % size],
-      this.currentGrid[(size - (x + 1)) % size][(size - y) % size], this.currentGrid[(size - (x - 1)) % size][(size - (y + 1)) % size],
-      this.currentGrid[(size - x) % size][(size - (y + 1)) % size], this.currentGrid[(size - (x + 1)) % size][(size - (y + 1)) % size],
+      this.currentGrid[(size + (x - 1)) % size][(size + (y - 1)) % size], 
+      this.currentGrid[(size + x) % size][(size + (y - 1)) % size],
+      this.currentGrid[(size + (x + 1)) % size][(size + (y - 1)) % size], 
+      this.currentGrid[(size + (x - 1)) % size][(size + y) % size],
+      this.currentGrid[(size + (x + 1)) % size][(size + y) % size], 
+      this.currentGrid[(size + (x - 1)) % size][(size + (y + 1)) % size],
+      this.currentGrid[(size + x) % size][(size + (y + 1)) % size], 
+      this.currentGrid[(size + (x + 1)) % size][(size + (y + 1)) % size],
     ]
     }
 
--- src/models/grid/grid.test.js
+++ src/models/grid/grid.test.js
@@ -51,37 +51,47 @@ describe('Grid', () => {
         expect(test_grid_instance.render()).toEqual(test_grid)
     })
 
-    describe('Spinner', () => {
-      it('rotates a line of live cells by 90 degrees', () => {
-        let first_grid = [
+    it('it can take a cell at [0, 1], [1,1], [2,1]', () => {
+        test_grid = [
             ['-', '-', '-'],
             ['*', '*', '*'],
             ['-', '-', '-']
         ]
 
-        let second_grid = [
-            ['-', '*', '-'],
-            ['-', '*', '-'],
-            ['-', '*', '-']
-        ]
         test_grid_instance.place_cells([[0, 1], [1,1], [2,1]])
-        console.log(test_grid_instance.render())
-        test_grid_instance.evolve()
-        console.log(test_grid_instance.render())
-        expect(test_grid_instance.render()).toEqual(second_grid)
-      })
-
-      it('rotates a line of live cells by 180 degrees when evolve is called twice', () => {
-        let first_grid = [
-            ['-', '-', '-'],
-            ['*', '*', '*'],
-            ['-', '-', '-']
-        ]
-        test_grid_instance.place_cells([[0, 1], [1,1], [2,1]])
-        test_grid_instance.evolve()
-        test_grid_instance.evolve()
-        expect(test_grid_instance.render()).toEqual(first_grid)
-      })
+        expect(test_grid_instance.render()).toEqual(test_grid)
     })
 
+    describe('3 x 3 stripe', () => {
+        it('resurrects all cells after one evolution', () => {
+            let first_grid = [
+                ['-', '-', '-'],
+                ['*', '*', '*'],
+                ['-', '-', '-']
+            ]
+      
+            let second_grid = [
+                ['*', '*', '*'],
+                ['*', '*', '*'],
+                ['*', '*', '*']
+            ]
+            test_grid_instance.place_cells([[0, 1], [1,1], [2,1]])
+            console.log(test_grid_instance.render())
+            test_grid_instance.evolve()
+            console.log(test_grid_instance.render())
+            expect(test_grid_instance.render()).toEqual(second_grid)
+        })
+      
+        it('kills all cells after two evolutions', () => {
+            let first_grid = [
+                ['-', '-', '-'],
+                ['-', '-', '-'],
+                ['-', '-', '-']
+            ]
+            test_grid_instance.place_cells([[0, 1], [1,1], [2,1]])
+            test_grid_instance.evolve()
+            test_grid_instance.evolve()
+            expect(test_grid_instance.render()).toEqual(first_grid)
+        })
+    })
 })
