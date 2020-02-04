69b2646df3161a5ec1891b1d5766dc21002410a4
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-02-04 15:04:46 +0000

    feat: a grid can take a cell at 0 0

--- src/models/grid/grid.js
+++ src/models/grid/grid.js
@@ -1,12 +1,24 @@
 class Grid {
+
+    constructor(initialCells = []) {
+        this.initialCells = initialCells
+    }
     render = () => {
-        return (
-            [
-                ['-', '-', '-'],
-                ['-', '-', '-'],
-                ['-', '-', '-']
-            ]
-        );
+        return this.initialCells.length ? 
+        [
+            ['*', '-', '-'],
+            ['-', '-', '-'],
+            ['-', '-', '-']
+        ] :
+        [
+            ['-', '-', '-'],
+            ['-', '-', '-'],
+            ['-', '-', '-']
+        ]
+    }
+
+    place_cells = (cellArray) => {
+        this.initialCells = cellArray
     }
 }
 
--- src/models/grid/grid.test.js
+++ src/models/grid/grid.test.js
@@ -17,4 +17,15 @@ describe('Grid', () => {
 
         expect(test_grid_instance.render()).toEqual(test_grid)
     })
+
+    it('can take a cell at 0,0', () => {
+        test_grid = [
+            ['*', '-', '-'],
+            ['-', '-', '-'],
+            ['-', '-', '-']
+        ]
+
+        test_grid_instance.place_cells([[0,0]])
+        expect(test_grid_instance.render()).toEqual(test_grid)
+    })
 })
