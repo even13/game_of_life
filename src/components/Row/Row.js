c44a516cbe6e4f0422f698387153ddea844668af
Eve Noirault <student@Makerss-MacBook-Air.local>
2020-02-05 14:19:27 +0000

    feat: renders 3 cells when passed 3 cells

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -3,7 +3,10 @@ import React from 'react'
 class Row extends React.Component {
     render() {
         return (
-            <div data-test='component-row'></div>
+            <div data-test='component-row'>
+            {this.props.cells}
+
+            </div>
         )
     }
 }
--- src/components/Row/Row.test.js
+++ src/components/Row/Row.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Enzyme, {shallow} from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 import Row from './Row'
+import Cell from '../Cell/Cell'
 
 Enzyme.configure({adapter: new EnzymeAdapter()})
 
@@ -16,5 +17,15 @@ describe("<Row />", () => {
 
     it ("renders without error", () => {
         expect(RowComponent).toHaveLength(1)
+    })
+
+    it ("renders 3 cells when passed 3 cells", () => {
+      wrapper = shallow(<Row cells={[
+        <Cell key={1} data-test={'component-cell'}/>,
+        <Cell key={2} data-test={'component-cell'}/>,
+        <Cell key={3} data-test={'component-cell'}/>
+      ]}/>)
+        let cellComponent = wrapper.find("[data-test='component-cell']")
+        expect(cellComponent).toHaveLength(3)
     })
 })
