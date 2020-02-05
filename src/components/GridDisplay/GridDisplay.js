34d47eb4cccc65a762e226943f810207643a83b3
Raluca Ciucu <raluca.ciucu@gmail.com>
2020-02-05 12:41:40 +0000

    render a 3-row grid correctly

--- src/components/GridDisplay/GridDisplay.js
+++ src/components/GridDisplay/GridDisplay.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import Row from '../Row/Row'
 
 class GridDisplay extends React.Component {
+  state = {
+    data: []
+  }
+
+  renderGrid = () => {
+    const res = []
+    this.state.data.forEach((_, i) => {
+        res.push(
+          <Row 
+            key={`${i}index`} 
+            data-test='component-row' />
+        )
+    })
+
+    return res
+  }
   render() {
     return (
-      <div data-test='component-grid-display'></div>
+      <div data-test='component-grid-display'>
+        {this.renderGrid()}
+      </div>
     )
   }
 }
--- src/components/GridDisplay/GridDisplay.test.js
+++ src/components/GridDisplay/GridDisplay.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Enzyme, {shallow} from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 import GridDisplay from './GridDisplay'
+import Row from '../Row/Row'
 
 Enzyme.configure({adapter: new EnzymeAdapter()})
 
@@ -16,5 +17,15 @@ describe("<GridDisplay />", () => {
 
   it ("renders without error", () => {
     expect(GridDisplayComponent).toHaveLength(1)
+  })
+
+  it ("can display 3 rows correctly when passed in 3 rows", () => {
+    wrapper = shallow(<GridDisplay />)
+    wrapper.setState({data: [1, 1, 1]})
+    
+    const RowComponent = wrapper.find("[data-test='component-row']")
+    console.log(wrapper.debug())
+    expect(RowComponent).toHaveLength(3)
+
   })
 })
