2913d708fa102207c34911035deffe844cacf176
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-02-09 10:15:28 +0000

    feat: Shape returns coordinates for veryical spinner at [0, 1]

--- src/models/shape.js
+++ src/models/shape.js
@@ -1,5 +1,6 @@
 class Shape {
-  static create = () => {
+  static create = (type, coord) => {
+    if (JSON.stringify(coord) === JSON.stringify([0, 1])) return [[0, 0], [0, 1], [0, 2]];
     return [[2, 1], [2, 2], [2, 3]];
   }
 }
--- src/models/shape.test.js
+++ src/models/shape.test.js
@@ -4,4 +4,8 @@ describe('Shape', () => {
   it('returns coordinates for a vertical spinner at [2, 2]', () => {
     expect(Shape.create('spinner', [2, 2])).toEqual([[2, 1], [2, 2], [2, 3]]);
   });
+
+  it('returns coordinates for a vertical spinner at [0, 1]', () => {
+    expect(Shape.create('spinner', [0, 1])).toEqual([[0, 0], [0, 1], [0, 2]]);
+  });
 });
