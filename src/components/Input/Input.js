49ac34f2821848e1c6d1ebcd097bb3e5271ff5d2
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-02-11 21:45:20 +0000

    feat: <Input /> typ decided by props passed from form

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,9 +1,12 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 
 class Input extends React.Component {
   render() {
     return (
-      <input type="text" data-test="component-input" />
+      <div data-test="component-input">
+        <input type={this.props.type} data-test="input" />
+      </div>
     );
   }
 }
--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -18,4 +18,16 @@ describe('<Input />', () => {
   it('renders without error', () => {
     expect(inputComponent).toHaveLength(1);
   });
+
+  it(`renders a text input if its type prop === input`, () => {
+    wrapper = setup(Input, { type: 'text'});
+    const input = findByTestAttr(wrapper, 'input');
+    expect(input.prop('type')).toEqual('text');
+  });
+
+  it(`renders a range input if its type prop === range`, () => {
+    wrapper = setup(Input, { type: 'range'});
+    const input = findByTestAttr(wrapper, 'input');
+    expect(input.prop('type')).toEqual('range');
+  });
 });
