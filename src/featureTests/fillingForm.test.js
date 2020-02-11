c1d64cfd1ceda8990bcc295b5c924d8d960b3892
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-02-11 22:37:10 +0000

    feat: more player name testing

--- src/featureTests/fillingForm.test.js
+++ src/featureTests/fillingForm.test.js
@@ -19,7 +19,18 @@ describe('fillingForm', () => {
     const playerOneNameField = wrapper.find({ id: 'playerOneName' });
     const playerTwoNameField = wrapper.find({ id: 'playerTwoName' });
     await playerOneNameField.simulate('change', { target: { value: 'Sam' } });
-    await playerTwoNameField.simulate('change', { target: { value: 'Sam' } });
+    await playerTwoNameField.simulate('change', { target: { value: 'Andrew' } });
+    setTimeout(() => {
+      expect(wrapper.state('gameForm').playerOneName.value).toEqual('Sam');
+      expect(wrapper.state('gameForm').playerTwoName.value).toEqual('Andrew');
+    });
+  });
+
+  it('takes information for two players named Eve and Raluca', async () => {
+    const playerOneNameField = wrapper.find({ id: 'playerOneName' });
+    const playerTwoNameField = wrapper.find({ id: 'playerTwoName' });
+    await playerOneNameField.simulate('change', { target: { value: 'Eve' } });
+    await playerTwoNameField.simulate('change', { target: { value: 'Raluca' } });
     setTimeout(() => {
       expect(wrapper.state('gameForm').playerOneName.value).toEqual('Sam');
       expect(wrapper.state('gameForm').playerTwoName.value).toEqual('Andrew');
