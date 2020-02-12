92fec12d44fba6f8c570e4ffe23e3fa780fd04ea
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-02-12 10:39:51 +0000

    feat: create <FormPage /> component

---
+++ src/containers/FormPage/FormPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class FormPage extends React.Component {
+  state = {}
+
+  render() {
+    return (
+      <div data-test="component-form-page" />
+    );
+  }
+}
+
+export default FormPage;
